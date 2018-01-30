358c8c6f5e9e05ed1c6683ce1ce260f1ddc12c30
Jin <jinchulann93@gmail.com>
2018-01-30 09:28:41 +0900

    included main.cpp; a simple Hello World! code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cpp
 for practice and learning c++
+
+main.cpp
+  simple Hello World program
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  //include i/o
+
+using namespace std; //precompiled standard library; in this file for cout
+
+/*
+  main function; could have 'void' under parameter
+*/
+int main () {
+  cout << "Hello World!" << endl;
+  //cout - println; '<<' insertion operator; endl - end line / newline
+
+  return 0;
+}
